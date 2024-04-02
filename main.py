341e1654ff4a0e896516d21d47cf6c36986ac599
Olga Vishnievska <olgacherry03@gmail.com>
2024-04-03 00:56:06 +0300

    Added subtask 4

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit, fsolve, minimize
+from scipy.optimize import minimize
 from sympy import *
 
 
-def profit(x):
+def profit(x, price, func, w_k_func, w_l_func):
     k_val, l_val = x
-    return (p*(b0*k_val**b1*l_val**b2)-(w_k*k_val+w_l*l_val))
+    return price * func.subs({k: k_val, l: l_val}) - (w_k_func * k_val + w_l_func * l_val)
 
 
-def max_for_minimize(x):
-    return -profit(x)
+def max_for_minimize(x, price, func, w_k_func, w_l_func):
+    return -profit(x, price, func, w_k_func, w_l_func)
+
+
+def p_monopoly(q):
+    return 800000 - 4*q
+
+
+def w_k_monopsony(k_val):
+    return 20000 + 2 * k_val
+
+
+def w_l_monopsony(l_val):
+    return 17000 + l_val
 
 
 if __name__ == '__main__':
@@ -54,25 +65,44 @@ if __name__ == '__main__':
     b_coef = exp(b)/(1/exp(a))
     print(f"-(d({a})/d(log(MP_k/MP_l)))  = -(d({a})/d({b}))  =  -(d({a_sub})/d({ln(b_coef)-t1}))  =  "
           f"\n-(d({a_sub.subs(t1, (ln(b_coef)-t2))})/d({t2}))  =  {-diff(a_sub.subs(t1, (ln(b_coef)-t2)), t2)}")
+    print()
 
-    # Optimal factor under conditions of perfect competition
-    p = 9
-    w_k = 2
-    w_l = 3
+    # Optimal factors under conditions of perfect competition
+    p = 7
+    w_k = 3
+    w_l = 5
 
     bounds = [(0, None), (0, None)]
-    result = minimize(max_for_minimize, [5000, 13000], bounds=bounds)
-    K_opt, L_opt = result.x
-    print(K_opt, L_opt)
-    print(profit([K_opt, L_opt]))
+    result = minimize(max_for_minimize, x0=np.array([30000, 30000]), args=(p, F_func, w_k, w_l), bounds=bounds)
+    K_opt_long, L_opt_long = result.x
+    print("Long-term period:")
+    print("K*, L*: ", K_opt_long.round(5), L_opt_long.round(5))
+    print("Profit: ", profit([K_opt_long, L_opt_long], p, F_func, w_k, w_l))
+    print()
 
+    constraints = ({'type': 'ineq',
+                    'fun': lambda x: 30000 - x[0] - x[1]})
+    result = minimize(max_for_minimize, x0=np.array([30000, 30000]), args=(p, F_func, w_k, w_l), constraints=constraints)
+    K_opt_short, L_opt_short = result.x
+    print("Short-term period")
+    print("K*, L*: ", K_opt_short.round(5), L_opt_short.round(5))
+    print("Profit: ", profit([K_opt_short, L_opt_short], p, F_func, w_k, w_l))
+    print()
 
-    constraints = ({'type': 'ineq', 'fun': lambda x: 30000 - x[0] - x[1]})
-    result = minimize(max_for_minimize, [5000, 13000], constraints=constraints)
-    K_opt, L_opt = result.x
-    print(K_opt, L_opt)
-    print(profit([K_opt, L_opt]))
-
-
-
-
+    #optimal factors under monopoly-monopsony conditions
+    result = minimize(lambda x: max_for_minimize(x, p_monopoly(F_func.subs({k: x[0], l: x[1]})), F_func,
+                                                 w_k_monopsony(x[0]), w_l_monopsony(x[1])), x0=np.array([20000, 20000]),
+                      bounds=bounds)
+    K_opt_mono, L_opt_mono = result.x
+    print("term period")
+    print("K*, L*: ", K_opt_mono.round(5), L_opt_mono.round(5))
+    w_l_monopsony = w_l_monopsony(L_opt_mono)
+    w_k_monopsony = w_k_monopsony(K_opt_mono)
+    q = F_func.subs({k: K_opt_mono, l: L_opt_mono})
+    price = p_monopoly(q)
+    print("Price K: ", w_k_monopsony.round(5))
+    print("Price L: ", w_l_monopsony.round(5))
+    print("Price: ", price.round(5))
+    print("Quantity: ", q)
+    print("Profit: ", profit([K_opt_mono, L_opt_mono], price, F_func,
+                                                 w_k_monopsony, w_l_monopsony))
